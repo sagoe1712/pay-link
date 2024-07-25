953efd716faf688016da88ab646f24469bc4b5ae
Olayinka Sagoe <sagoe.olayinka@gmail.com>
2024-07-25 23:12:29 +0100

    entities003: add health to each character class

---
+++ entities/warriorClass.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace entities;
+
+Class warriorClass extends baseCharacterClass {
+    public function __construct()
+    {
+        $this->health = 100;
+    }
+
+    public function attack()
+    {
+        // TODO: Implement attack() method.
+    }
+
+    public function defend($damage)
+    {
+        // TODO: Implement defend() method.
+    }
+}
